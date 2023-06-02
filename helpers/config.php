64fbef91b3618ea1158ee0115cc23c0b87a041ed
Rijal Asep Nugroho <rijal.asep.nugroho@gmail.com>
2023-06-02 08:25:20 +0700

    Add files via upload

---
+++ helpers/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$DB_host = '';
+$DB_user = '';
+$DB_pass = '';
+$DB_dbname = '';
+
+$APPNAME = '';
+$SENDGRID = [
+  'from' => '',
+  'template_id' => '',
+  'cs_email' => '',
+  'cs_phone' => '',
+  'api_key' => ''
+];
+  
